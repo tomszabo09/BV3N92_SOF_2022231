868c8485fa7aa8b0c9cf8581d4fa36cb6fd91d44
Rajnai Gábor <gabika.hun@gmail.com>
2022-12-16 16:25:37 +0100

    Added a main page for visitors. I will need to fix the css behind index.cshtml New Controller added for Main.cshtml Next Project is to work on Login and Register Partial

--- BV3N92_SOF_2022231/Controllers/HomeController.cs
+++ BV3N92_SOF_2022231/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Backend.Controllers
             return View();
         }
 
+        public IActionResult Main()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
