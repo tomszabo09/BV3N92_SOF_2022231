dca43a7f83a69cd9f44a534b17996dd3f22858bd
Tamas Szabo <tamas.szabo@dmgmori-heitec.hu>
2022-12-20 10:44:45 +0100

    added admin controller

---
+++ BV3N92_SOF_2022231/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult ControlPanel()
+        {
+            return View();
+        }
+
+        public IActionResult ManageAll()
+        {
+            return View();
+        }
+    }
+}
