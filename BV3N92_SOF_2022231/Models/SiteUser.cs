cbea499f734f917530324f0a741231b15506fdcb
csonor <csdobak2@gmail.com>
2023-01-18 11:09:53 +0100

    Modified attributes and formatting

--- BV3N92_SOF_2022231/Models/SiteUser.cs
+++ BV3N92_SOF_2022231/Models/SiteUser.cs
@@ -4,66 +4,52 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Backend.Models
 {
-    public enum Gender
-    {
-        Female, Male, NonBinary
-    }
+	public enum Gender
+	{
+		Female, Male, NonBinary
+	}
 
-    public enum Orientation
-    {
-        Straight,Gay, Lesbian, Bisexual, Heterosexual, NonBinary,Asexual,Pansexual,Queer
-    }
-    public enum Education
-    {
-        None, Elementary, HighSchool, Bachelor, Master, PhD
-    }
-    public class SiteUser : IdentityUser
-    {
-        public string Id { get; set; }
+	public enum Orientation
+	{
+		Straight, Gay, Lesbian, Bisexual, Heterosexual, NonBinary, Asexual, Pansexual, Queer
+	}
+	public enum Education
+	{
+		None, Elementary, HighSchool, Bachelor, Master, PhD
+	}
 
-        [Required]
-        [StringLength(20)]
-        public string FirstName { get; set; }
+	public class SiteUser : IdentityUser
+	{
+		[Key]
+		public string UId { get; set; }
 
-        [Required]
-        [Range(18, int.MaxValue)]
-        public int Age { get; set; }
+		[Required]
+		[StringLength(20)]
+		public string FirstName { get; set; }
 
-        [Required]
-        public Orientation Orientation { get; set; }
+		[Required]
+		[Range(18, int.MaxValue)]
+		public int Age { get; set; }
 
-        [Required]
-        public Gender Gender { get; set; }
+		[Required]
+		public Orientation Orientation { get; set; }
 
-        [StringLength(500)]
-        public string Bio { get; set; }
-        
-        [Required]
-        public Picture ProfilePicture { get; set; }
+		[Required]
+		public Gender Gender { get; set; }
 
-        [NotMapped]
-        public ICollection<Picture> Pictures { get; set; }
+		[StringLength(500)]
+		public string Bio { get; set; }
 
-        public SiteUser()
-        {
-            Id = Guid.NewGuid().ToString();
-            this.Pictures = new List<Picture>();
-        }
-    }
-    public class Picture
-    {
-        [ForeignKey(nameof(User))]
-        public int UserId { get; set; }
-        [NotMapped]
-        public virtual SiteUser User { get; set; }
-        [Key]
-        public string PictureId { get; set; }
-        [Required]
-        public string PhotoUrl { get; set; }
+		[Required]
+		public Picture ProfilePicture { get; set; }
 
-        public Picture()
-        {
-            PictureId = Guid.NewGuid().ToString();
-        }
-    }
+		[NotMapped]
+		public ICollection<Picture> Pictures { get; set; }
+
+		public SiteUser()
+		{
+			UId = Guid.NewGuid().ToString();
+			this.Pictures = new List<Picture>();
+		}
+	}
 }
