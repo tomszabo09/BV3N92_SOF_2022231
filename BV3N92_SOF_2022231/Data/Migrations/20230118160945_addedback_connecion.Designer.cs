df26168d288ffe690c4d2422821e277364f7cb4a
csonor <csdobak2@gmail.com>
2023-01-18 17:10:16 +0100

    added connection back

--- BV3N92_SOF_2022231/Data/Migrations/20230118160945_addedback_connecion.Designer.cs
+++ BV3N92_SOF_2022231/Data/Migrations/20230118160945_addedback_connecion.Designer.cs
@@ -4,6 +4,7 @@ using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230118160945_addedback_connecion")]
+    partial class addedback_connecion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
