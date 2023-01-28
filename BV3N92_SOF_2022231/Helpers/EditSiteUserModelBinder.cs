81be338b6f3b7440d98d11ef5c1c6ba45b7e23d1
csonor <csdobak2@gmail.com>
2023-01-28 16:10:47 +0100

    edit custom pictures

--- BV3N92_SOF_2022231/Helpers/EditSiteUserModelBinder.cs
+++ BV3N92_SOF_2022231/Helpers/EditSiteUserModelBinder.cs
@@ -9,27 +9,27 @@ namespace Backend.Helpers
 	{
 		public Task BindModelAsync(ModelBindingContext bindingContext)
 		{
-			if (bindingContext == null)
-			{
-				throw new ArgumentNullException(nameof(bindingContext));
-			}
+			//	if (bindingContext == null)
+			//	{
+			//		throw new ArgumentNullException(nameof(bindingContext));
+			//	}
 
-			EditSiteUser user = new EditSiteUser();
-			user.FirstName = bindingContext.ValueProvider.GetValue("firstname").FirstValue;
-			user.Age = int.Parse(bindingContext.ValueProvider.GetValue("age").FirstValue);
-			user.Bio = bindingContext.ValueProvider.GetValue("bio").FirstValue;
-			user.Gender = (Gender)int.Parse(bindingContext.ValueProvider.GetValue("gender").FirstValue);
-			user.Orientation = (Orientation)int.Parse(bindingContext.ValueProvider.GetValue("orientation").FirstValue);
+			//	EditSiteUser user = new EditSiteUser();
+			//	user.FirstName = bindingContext.ValueProvider.GetValue("firstname").FirstValue;
+			//	user.Age = int.Parse(bindingContext.ValueProvider.GetValue("age").FirstValue);
+			//	user.Bio = bindingContext.ValueProvider.GetValue("bio").FirstValue;
+			//	user.Gender = (Gender)int.Parse(bindingContext.ValueProvider.GetValue("gender").FirstValue);
+			//	user.Orientation = (Orientation)int.Parse(bindingContext.ValueProvider.GetValue("orientation").FirstValue);
 
-			if (bindingContext.HttpContext.Request.Form.Files.Count > 0)
-			{
-				user.ProfilePicture = bindingContext.HttpContext.Request.Form.Files[0];
-				for (int i = 1; i < bindingContext.HttpContext.Request.Form.Files.Count; i++)
-				{
-					user.UserPictures.Add(bindingContext.HttpContext.Request.Form.Files[i]);
-				}
-			}
-			bindingContext.Result = ModelBindingResult.Success(user);
+			//	if (bindingContext.HttpContext.Request.Form.Files.Count > 0)
+			//	{
+			//		user.ProfilePicture = bindingContext.HttpContext.Request.Form.Files[0];
+			//		for (int i = 1; i < bindingContext.HttpContext.Request.Form.Files.Count; i++)
+			//		{
+			//			user.CurrentPicturesUpdates.Add(bindingContext.HttpContext.Request.Form.Files[i]);
+			//		}
+			//	}
+			//	bindingContext.Result = ModelBindingResult.Success(user);
 			return Task.CompletedTask;
 		}
 	}
