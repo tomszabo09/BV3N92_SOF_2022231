8ceb90fedda88a40a436b6f7ff42984bf0161ac3
tomt09 <tom.szabo09@gmail.com>
2023-01-29 18:41:31 +0100

    started working on match notification

---
+++ BV3N92_SOF_2022231/Models/MatchAlertModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Models
+{
+	public class MatchAlertModel
+	{
+		public string MatchId { get; set; }
+
+		public MatchAlertModel(string userId)
+		{
+			this.MatchId = userId;
+		}
+	}
+}
