79b553d672793ee5412c96a3f3793bc63f2d6b2e
csonor <csdobak2@gmail.com>
2023-01-31 17:27:57 +0100

    added delete user notifications

--- BV3N92_SOF_2022231/Controllers/HomeController.cs
+++ BV3N92_SOF_2022231/Controllers/HomeController.cs
@@ -197,6 +197,9 @@ namespace Backend.Controllers
 
 			await _signInManager.SignOutAsync();
 			await _userManager.DeleteAsync(user);
+			
+			await _hub.Clients.All.SendAsync("userDeleted");
+
 			return RedirectToAction(nameof(Visitor));
 		}
 
